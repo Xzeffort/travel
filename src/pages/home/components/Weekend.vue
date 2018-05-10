7a7e0d1f752add25f5c36b477d121b69476b6ac9
CookiesEason <837447352@qq.com>
2018-05-10 13:46:48 +0800

    add recommed

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <div class="title">周末去哪儿</div>
+        <ul>
+          <li class="item border-bottom" v-for="item in recommendList" :key="item.id">
+             <div class="item-img-wrapper">
+                  <img class="item-img" :src="item.imgUrl"/>
+             </div>
+            <div class="item-info">
+              <p class="item-title">{{item.title}}</p>
+              <p class="item-desc">{{item.desc}}</p>
+            </div>
+          </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'WeekendRecommend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/92/3e980b7fdb0c92.jpg_r_640x214_f3bf810d.jpg',
+          title: '西溪国家湿地公园',
+          desc: '里面的动物太少了,里面的动物太少了'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/92/3e980b7fdb0c92.jpg_r_640x214_f3bf810d.jpg',
+          title: '西溪国家湿地公园',
+          desc: '里面的动物太少了,里面的动物太少了'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/92/3e980b7fdb0c92.jpg_r_640x214_f3bf810d.jpg',
+          title: '西溪国家湿地公园',
+          desc: '里面的动物太少了,里面的动物太少了'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl';
+    .title
+      margin-top .2rem
+      line-height .8rem
+      background #eeeeee
+      text-indent .2rem
+    .item-img-wrapper
+      overflow hidden
+      height 0
+      padding-bottom 33.9%
+      .item-img
+        width 100%
+    .item-info
+      padding .1rem
+      .item-title
+          line-height .54rem
+          font-size .32rem
+          ellipsis()
+      .item-desc
+          line-height .4rem
+          color #ccc
+          ellipsis()
+</style>
